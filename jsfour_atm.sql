c11786bb8cec52e5baa6f2ef6e204a1367e47131
Tinky <48660658+Tinky124@users.noreply.github.com>
2020-07-16 15:00:01 +0200

    Update jsfour_atm.sql

--- jsfour_atm.sql
+++ jsfour_atm.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `jsfour_atm` (
-  `identifier` varchar(40) NOT NULL,
-  `account` varchar(60) NOT NULL,
-  `pincode` int(11) NOT NULL DEFAULT '1111',
+  `identifier` VARCHAR(40) NOT NULL,
+  `account` VARCHAR(60) NOT NULL,
+  `pincode` INT(11) NOT NULL DEFAULT '1111',
    PRIMARY KEY (`identifier`)
 );
